ed984d1e73fb3fd872977afa484b69ab07829444
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-07-09 04:33:39 +0530

    Imported Cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import morgan from "morgan";
 import authRoute from "./routes/authRoute.js";
+import cors from "cors";
 
 //configure env
 dotenv.config();
@@ -15,21 +16,22 @@ connectDB();
 const app = express();
 
 //middlewares
+app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 //routes
-app.use('/api/v1/auth', authRoute);
+app.use("/api/v1/auth", authRoute);
 
 //rest-api
-app.get('/', (req, res)=> {
-    res.send('<h1>Welcome to Ecommerce App</h1>');
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Ecommerce App</h1>");
+});
 
 //PORT
 const PORT = process.env.PORT;
 
 //run listen
-app.listen(PORT, ()=>{
-    console.log(`Server is listening on PORT ${PORT}`.bgCyan.white);
-})
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`.bgCyan.white);
+});
