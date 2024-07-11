17aa166f99456840598d4a0c60686856ee6b6606
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-07-12 03:00:10 +0530

    Product API CRUD with image upload

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import morgan from "morgan";
 import authRoute from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
 //configure env
@@ -24,6 +25,7 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 //rest-api
 app.get("/", (req, res) => {
