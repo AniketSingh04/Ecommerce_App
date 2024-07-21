2231f1f636d2457fa5293a973d083e5211d7e526
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-07-22 02:07:09 +0530

    Route Changed for delete product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -42,6 +42,6 @@ router.get("/get-product/:slug", getSingleProductController);
 router.get("/product-photo/:pid", productPhotoController);
 
 //delete router
-router.delete("/product/:pid", deleteProductController);
+router.delete("/delete-product/:pid", deleteProductController);
 
 export default router;
