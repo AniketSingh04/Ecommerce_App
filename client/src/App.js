445696945ce98f1e6adffe2f83ffd4f7b41ffa47
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-08-13 00:57:45 +0530

    Components added and CSS upodated for some sections

--- client/src/App.js
+++ client/src/App.js
@@ -23,13 +23,17 @@ import Products from "./pages/Admin/Products";
 import UpdateProduct from "./pages/Admin/UpdateProduct";
 import Search from "./pages/Search";
 import ProductDetails from "./pages/ProductDetails";
+import Categories from "./pages/Categories";
+import CategoryProoduct from "./pages/CategoryProoduct";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/product/:slug" element={<ProductDetails />} /> .
+        <Route path="/product/:slug" element={<ProductDetails />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/category/:slug" element={<CategoryProoduct />} /> .
         <Route path="/search" element={<Search />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard />} />
