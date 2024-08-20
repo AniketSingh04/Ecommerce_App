baf0c8d0f01b132531de1c508d22c5cfa914b85a
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-08-20 17:27:11 +0530

    Shopping cart and User Profile

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ import Search from "./pages/Search";
 import ProductDetails from "./pages/ProductDetails";
 import Categories from "./pages/Categories";
 import CategoryProoduct from "./pages/CategoryProoduct";
+import CartPage from "./pages/CartPage";
 
 const App = () => {
   return (
@@ -33,7 +34,8 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/product/:slug" element={<ProductDetails />} />
         <Route path="/categories" element={<Categories />} />
-        <Route path="/category/:slug" element={<CategoryProoduct />} /> .
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/category/:slug" element={<CategoryProoduct />} />
         <Route path="/search" element={<Search />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard />} />
--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,17 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/auth";
 import { SearchProvider } from "./context/search";
+import { CartProvider } from "./context/cart";
 import "antd/dist/reset.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
     <SearchProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
     </SearchProvider>
   </AuthProvider>
 );
